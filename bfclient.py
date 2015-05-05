8cf5ffa5162397e42534a121acf3a81d76758fb9
James Lin <jl3782@columbia.edu>
2015-05-05 01:01:38 -0400

    feat(*): route update converges successfully

--- bfclient.py
+++ bfclient.py
@@ -12,45 +12,219 @@ import sys
 import datetime
 import time
 import threading
+import rt_packet
 from multiprocessing import Lock
 
 # instantiate global variables, values of this host
+'''
+neighbors - the host's one-hop neighbors
+my_dvs - this consists of the host's routing table
+dv_tables - the routing tables of all other nodes in network
+original_neighbors - original neighbor values
+'''
 ip_port = ('', 0)
 udp_sock = None
 my_dvs = []
+neighbors = []
 dv_tables = {}
-original_dv_dic = {}
-TIMEOUT = 30
+original_neighbors = {}
+TIMEOUT = 0
 lock = Lock()
 dead = False
 HOSTNAME = ''
+
+# update the paths, returns True if paths have actually been changed
+def thread_update_paths():
+    global lock
+    global dv_tables
+    global my_dvs
+    global HOSTNAME
+    updated = False
+    lock.acquire()
+    try:
+        # go through entire dictionary
+        for host, lst in dv_tables.iteritems():
+            # ignore yourself
+            if host == HOSTNAME:
+                continue
+            # record the link cost
+            neighbor_cost = find_neighbor_value(host)
+            
+            # go through each tuple
+            for tup in lst:
+                if tup[0] == HOSTNAME:
+                    continue
+
+                # if this is the first time, go ahead and record
+                if not is_recorded(tup[0]):
+                    dv_tables[HOSTNAME].append((tup[0], tup[1] + 
+                        neighbor_cost, host))
+                    updated = True
+                else:
+                    # see what the potential path cost is
+                    new_path_cost = neighbor_cost + tup[1]
+                    least = 0
+                    table_record = ()
+                
+                    # go through and find the least cost thus far
+                    for node in dv_tables[HOSTNAME]:
+                        if node[0] == tup[0]: 
+                            least = node[1]
+                            table_record = node
+                            break
+                
+                    # if new cost is less, then pick the new path path
+                    if new_path_cost < least:
+                        dv_tables[HOSTNAME].remove(table_record)
+                        dv_tables[HOSTNAME].append((tup[0], new_path_cost,
+                            host))
+                        updated = True
+                
+    finally:
+        lock.release()
+        return updated
+
+
+# update row on dv_table
+def thread_update_dv_tables(host, new_row):
+    global lock
+    global dv_tables
+    lock.acquire()
+    try:
+        dv_tables[host] = new_row
+    finally:
+        lock.release()
+
+# handle the packet that was received
+def handle_recv_packet(data):
+    global dead
+    global neighbors
+    global HOSTNAME
+    global my_dvs
+    global dv_tables
+
+    # parse the data
+    data_arr = data.split('&')
+    sender = data_arr[0]
+    code = data_arr[1]
+    
+    if code == 'ROUTEUPDATE':
+        # retrieve the data and get rid of delimeter
+        row = data_arr[2].split('/')
+        tup_row = []
+        for i in range(0, len(row)-1):
+            result = eval(row[i])
+            if type(result) is tuple:
+                tup_row.append(result)
+
+        thread_update_dv_tables(sender, tup_row)
+        updated = thread_update_paths()
+        my_dvs = dv_tables[HOSTNAME]
+
+        if updated and not dead:
+            for neighbor in neighbors:
+                send_packet(neighbor[0], 
+                    rt_packet.RTPacket('ROUTEUPDATE', HOSTNAME, my_dvs))
+
+    return 0
+
+# manager thread 
+def listener_thread():
+    global udp_sock
+
+    while True:
+        d = udp_sock.recvfrom(4096)
+        data = d[0]
+        server = threading.Thread(target=handle_recv_packet, args=(data,))
+        server.start()
+
 
 # timeout function
 def timeout_function():
     global TIMEOUT
     global dead
-    # need to fill this out
+    global neighbors
+    global HOSTNAME
+    global my_dvs
     time.sleep(TIMEOUT)
+
+    # only send ROUTEUPDATE if alive
     if not dead:
-        print 'TIMEOUT'
+        # send to all neighbors
+        for neighbor in neighbors:
+            # adjust for poison reverse
+            # custom my_dvs based on how routing happens
+            # change what you report but not actual my_dvs
+            send_packet(neighbor[0], 
+                rt_packet.RTPacket('ROUTEUPDATE', HOSTNAME, my_dvs))
 
     t = threading.Thread(target=timeout_function)
     t.daemon = True
     t.start()
     return(0)
 
+'''
+destination - destination to send in str format <IP>:<local port>
+packet - packet we actually want to send 
+'''
+def send_packet(destination, packet):
+    # send the actual packet
+    # create the socket
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error:
+        print 'Failed to create socket'
+        sys.exit()
+
+    dest_ip_port = destination.split(':')
+    sock.sendto(str(packet), (dest_ip_port[0], int(dest_ip_port[1])))
+    sock.close()
+    # actually send the stuff
+
+
 # changes a dv to a certain value, avoids race conditions
-def thread_change_dv(dv_to_change, new_value):
+def thread_change_dv_row(dv_to_change, new_value, new_link=''):
     global lock
     global my_dvs
     lock.acquire()
     try:
         for dv in my_dvs:
             if dv[0] == dv_to_change:
+                new_dv = ()
+                if new_link == '':
+                    new_dv = (dv_to_change, new_value, dv[2])
+                else:
+                    new_dv = (dv_to_change, new_value, new_link)
                 my_dvs.remove(dv)
-                my_dvs.append((dv_to_change, new_value))
+                my_dvs.append(new_dv)
     finally:
         lock.release()
+
+# actual neighbors
+def change_neighbor(neighbor_to_change, new_value):
+    global neighbors
+
+    for neighbor in neighbors:
+        if neighbor[0] == neighbor_to_change:
+            new_neighbor = (neighbor_to_change, new_value)
+
+# find link value to neighbor
+def find_neighbor_value(neighbor_to_find):
+    global neighbors
+
+    for neighbor in neighbors:
+        if neighbor[0] == neighbor_to_find:
+            return neighbor[1]
+
+# see if link is recorded
+def is_recorded(to_find):
+    global dv_tables
+    global HOSTNAME
+
+    for node in dv_tables[HOSTNAME]:
+        if node[0] == to_find:
+            return True
+    return False
 
 # changes a dv to a certain value, avoids race conditions
 def thread_close_host():
@@ -60,16 +234,16 @@ def thread_close_host():
     new_dv_list = []
     try:
         for dv in my_dvs:
-            new_dv_list.append((dv[0], float('inf')))
+            new_dv_list.append((dv[0], float('inf'), 'None'))
         my_dvs = new_dv_list
     finally:
         lock.release()
 
 # checks to see if the dv is adjacent or not
 def in_original(dv_to_check):
-    global original_dv_dic
+    global original_neighbors
     try:
-        original_dv_dic[dv_to_check]
+        original_neighbors[dv_to_check]
     except Exception:
         return False
     return True
@@ -86,8 +260,9 @@ def main():
     global ip_port
     global udp_sock
     global my_dvs
+    global neighbors
     global dv_tables
-    global original_dv_dic
+    global original_neighbors
     global TIMEOUT
     global dead
     global HOSTNAME
@@ -118,21 +293,43 @@ def main():
             TIMEOUT = int(port_timeout[1])
         else:
             dest_cost = line.split()
-            my_dvs.append((dest_cost[0], float(dest_cost[1])))
-            original_dv_dic[dest_cost[0]] = float(dest_cost[1])
+            my_dvs.append((dest_cost[0], float(dest_cost[1]), dest_cost[0]))
+            neighbors.append((dest_cost[0], float(dest_cost[1])))
+            original_neighbors[dest_cost[0]] = float(dest_cost[1])
 
-    sock_name = socket.gethostbyname(socket.gethostname())
+    # sock_name = socket.gethostbyname(socket.gethostname())
     HOSTNAME = '127.0.0.1' + ':' + str(port)
 
     dv_tables[HOSTNAME] = my_dvs
-    print dv_tables
-    print ip_port
-    print TIMEOUT
-    print my_dvs
-    print original_dv_dic
+    # print dv_tables
+    # print ip_port
+    # print TIMEOUT
+    # print my_dvs
+    # print original_neighbors
 
     # launch the timeout thread
     t = threading.Thread(target=timeout_function)
+    t.daemon = True
+    t.start()
+
+    '''
+    UDP sock stuff
+    '''
+    try:
+        udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print 'Socket created'
+    except socket.error:
+        print 'Failed to create socket'
+        sys.exit()    
+
+    # bind UDP socket
+    try:
+        udp_sock.bind((ip_port[0], ip_port[1]))
+    except socket.error:
+        print 'Bind failed'
+        sys.exit()
+
+    t = threading.Thread(target=listener_thread)
     t.daemon = True
     t.start()
 
@@ -159,7 +356,7 @@ def main():
 
             for dv in my_dvs:
                 print ('Destination = ' + dv[0] + ', Cost = ' + str(dv[1]) + 
-                    ', Link = ()')
+                    ', Link = (' + dv[2] + ')')
         elif user_input[0] == 'LINKDOWN':
 
             if len(user_input) != 3:
@@ -169,7 +366,8 @@ def main():
             dv = user_input[1] + ':' + user_input[2]
 
             if in_original(dv):
-                thread_change_dv(dv, float('inf'))
+                change_neighbor(dv, float('inf'))
+                # set any path using link to inf, if so send ROUTEUPDATE
                 # send LINKDOWN signal
                 # stop exchanging ROUTE UPDATE messages
             else:
@@ -196,8 +394,9 @@ def main():
             dv = user_input[1] + ':' + user_input[2]
 
             if in_original(dv):
-                thread_change_dv(dv, original_dv_dic[dv])
-                # resume sending ROUTE UPDATE messages
+                change_neighbor(dv, original_neighbors[dv])
+                # should send LINKUP message to destination
+                # resume sending ROUTEUPDATE messages
             else:
                 print 'The host you entered is not your neighbor.'
         elif user_input[0] == 'CHANGECOST':
@@ -209,46 +408,10 @@ def main():
             dv = user_input[1] + ':' + user_input[2]
 
             if in_original(dv):
-                thread_change_dv(dv, float(user_input[3]))
+                change_neighbor(dv, float(user_input[3]))
             else:
                 print 'The host you entered is not your neighbor.'
 
-
-    '''
-
-    # create UDP socket
-
-    try:
-        udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        print 'Socket created'
-    except socket.error:
-        print 'Failed to create socket'
-        sys.exit()
-
-    
-    # bind UDP socket
-    try:
-        udp_sock.bind(('', 5000))
-    except socket.error:
-        print 'Bind failed'
-        sys.exit()
-
-    print 'Socket bind complete'
-
-    # serve, echo server for now
-    while 1:
-        d = udp_sock.recvfrom(1024)
-        data = d[0]
-        addr = d[1]
-
-        if not data:
-            break
-
-        reply = 'OK...' + data
-
-        udp_sock.sendto(reply, addr)
-        print 'replied'
-    '''
 
 # ^C terminate gracefully
 def ctrl_c_handler(signum, frame):
--- rt_packet.py
+++ rt_packet.py
@@ -1,6 +1,12 @@
 class RTPacket:
     'packets that can be sent back and forth'
 
+    '''
+    code - code to define function of packet
+    hostname - source in format <IP>:<local port>
+    dvs - for ROUTEUPDATE, list of my current dvs
+    link - the link that went down or up
+    '''
     def __init__(self, code, hostname, dvs=[], link = ''):
         self.code = code
         self.hostname = hostname
@@ -8,9 +14,12 @@ class RTPacket:
         self.link = link
 
     def __str__(self):
-        if self.code == 'ROUTEUDPATE':
-            return self.hostname + '\n' + 'ROUTEUDPATE' + '\n' + str(self.dvs)
+        if self.code == 'ROUTEUPDATE':
+            s = ''
+            for dv in self.dvs:
+                s = s + str(dv) + '/'
+            return self.hostname + '&' + self.code + '&' + s
         if self.code == 'LINKDOWN':
-            return self.hostname + '\n' + 'LINKDOWN' + '\n' + self.link
+            return self.hostname + '&' + self.code + '&' + self.link
         if self.code == 'LINKUP':
-            return self.hostname + '\n' + 'LINKUP'
+            return self.hostname + '&' + self.code
